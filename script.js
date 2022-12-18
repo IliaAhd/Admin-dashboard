d18adee82833d28dddea990b0fd68d5f513b5b79
IliaNoah <vaguaman86@gmail.com>
2022-12-18 17:41:34 +0330

    finished tables

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-const number = document.querySelectorAll("#number");
+const firstTableNumber = document.querySelectorAll("#first-number");
+const secondTableNumber = document.querySelectorAll("#second-number");
 const pageNumber = document.querySelectorAll("#page-number");
 
-for (let i = 1; i < number.length; i++) {
-  number[i].textContent = i + 1;
+for (let i = 1; i < firstTableNumber.length; i++) {
+  firstTableNumber[i].textContent = i + 1;
+}
+for (let i = 1; i < secondTableNumber.length; i++) {
+  secondTableNumber[i].textContent = i + 1;
 }
 
 for (let i = 1; i < pageNumber.length; i++) {
-    pageNumber[i].textContent = i + 1;
-  }
+  pageNumber[i].textContent = i + 1;
+}
